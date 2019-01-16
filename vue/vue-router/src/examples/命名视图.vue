f32faf3350334319f809364740a5d481edd6cf23
yaolin1 <yaolin1@xiaomi.com>
2019-01-16 10:34:25 +0800

    命名视图

---
+++ vue/vue-router/src/examples/命名视图.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <router-view></router-view>
+        <router-view name="foo"></router-view>
+        <router-view name="bar"></router-view>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import NotFound from '@/components/404';
+import Foo from '@/components/Foo';
+import Bar from '@/components/Bar';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        {path: '/', components: {
+            default: NotFound,
+            foo: Foo,
+            bar: Bar            
+        }},
+    ]
+})
+export default {
+    name: 'Start',
+    router
+}
+</script>
+
--- vue/vue-router/src/main.js
+++ vue/vue-router/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './examples/命名路由.vue'
+import App from './examples/命名视图.vue'
 
 Vue.config.productionTip = false
 
