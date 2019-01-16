f0d71f68d4e50a01798fdcca41ca89761e82ec81
yaolin1 <yaolin1@xiaomi.com>
2019-01-16 10:05:19 +0800

    嵌套路由

---
+++ vue/vue-router/src/components/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  Profile
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {},
+}
+</script>
+
+<style scoped>
+</style>
--- vue/vue-router/src/examples/嵌套路由.vue
+++ vue/vue-router/src/examples/嵌套路由.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
         <router-link to="/user/1">user1</router-link> <br/>
-        <router-link to="/user/2">user2</router-link> <br/>
-        <router-link to="/user/3">user3</router-link> <br/>
-        <router-link to="/user/4/post/1">user4-post1</router-link> <br/>
+        <router-link to="/user/2/profile">user2 profile</router-link> <br/>
+        <router-link to="/user/3/posts">user3 posts</router-link> <br/>
 
         <router-view></router-view>
     </div>
@@ -13,15 +12,33 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import User from '@/components/User';
-import UserPost from '@/components/UserPost';
+import User from '@/components/User1';
+import Profile from '@/components/Profile';
+import UserPosts from '@/components/UserPosts';
+import NotFound from '@/components/404';
+
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     routes: [
-        {path: '/user/:id', component: User},
-        {path: '/user/:id/post/:postId', component: UserPost},
+        {path: '/user/:id', component: User, 
+            children: [
+                {
+                    path: '',
+                    component: NotFound
+                },
+                {
+                    path: 'profile',
+                    component: Profile
+                },
+
+                {
+                    path: 'posts',
+                    component: UserPosts
+                }
+            ]
+        },
     ]
 })
 export default {
