e6b9364762678735be6e39840d275e5a261f7f66
yaolin1 <yaolin1@xiaomi.com>
2019-01-18 14:01:37 +0800

    mutation

---
+++ vue/vuex/src/examples/mutation.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        {{count}}
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+        <button @click="add({n:10})">add</button>
+    </div>
+</template>
+
+<script>
+
+import Vuex from 'vuex'
+import { mapMutations } from 'vuex'
+import Vue from 'vue'
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        count: 0,
+    }, 
+    mutations: {
+        increment: state => state.count ++,
+        decrement: state => state.count --,
+        add(state, payload){
+            state.count += payload.n;
+        }
+    }
+})
+export default {
+    store,
+    computed: {
+        count() {
+            return this.$store.state.count;
+        }
+    },
+    methods: {
+        // increment(){
+        //     store.commit('increment')
+        // },
+        // decrement(){
+        //     store.commit('decrement')
+        // },
+        // add(payload){
+        //     store.commit('add', payload)
+        // }
+
+
+        // ...mapMutations([
+        //     'increment',
+        //     'decrement',
+        //     'add',
+        // ])
+
+        ...mapMutations([
+            'increment',
+            'decrement',
+        ]),
+        ...mapMutations({
+            'add': 'add',
+        })
+    }
+}
+</script>
+
--- vue/vuex/src/main.js
+++ vue/vuex/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from '@/examples/mapGetter.vue'
+import App from '@/examples/mutation.vue'
 
 Vue.config.productionTip = false
 
