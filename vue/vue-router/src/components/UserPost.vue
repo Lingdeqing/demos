062eda4735ee0aefc2ffd507438c57dd0cf90cd6
yaolin1 <15851853085@163.com>
2019-01-20 17:24:55 +0800

    导航守卫和生命周期函数执行顺序

--- vue/vue-router/src/components/UserPost.vue
+++ vue/vue-router/src/components/UserPost.vue
@@ -11,7 +11,37 @@
 import User from '@/components/User';
 export default {
   name: 'Post',
-  props: {},
+  props: {
+    postId2: {
+      type: String,
+      default(){
+        console.log('props')
+        return ''
+      }
+    }
+  },
+  beforeRouteEnter (to, from ,next){
+    console.log('beforeRouteEnter')
+    next();
+  },
+  beforeRouteUpdate(to, from ,next){
+    console.log('beforeRouteUpdate')
+    next();
+  },
+  beforeRouteLeave(to, from ,next){
+    console.log('beforeRouteLeave')
+    next();
+  },
+
+  beforeCreate(){
+    console.log('beforeCreate')
+  },
+  updated(){
+    console.log('updated')
+  },
+  beforeDestroy(){
+    console.log('beforeDestroy')
+  },
   components: {
     User
   }
--- vue/vue-router/src/examples/导航守卫.vue
+++ vue/vue-router/src/examples/导航守卫.vue
@@ -4,12 +4,41 @@
         <router-link to="/user/2">user2</router-link> <br/>
         <router-link to="/user/3">user3</router-link> <br/>
         <router-link to="/user/4/post/1">user4-post1</router-link> <br/>
+        <router-link to="/user/4/post/2">user4-post2</router-link> <br/>
         <router-link to="/error">error</router-link> <br/>
         <router-link to="/foo/1">/foo/1</router-link> <br/>
         <router-link to="/foo/2">/foo/2</router-link> <br/>
         <router-link to="/foo/3">/foo/3</router-link> <br/>
 
         <router-view></router-view>
+        <h2>第一次或者由其他路由进入新的路由钩子顺序</h2>
+        <p>
+            beforeEach<br>
+            beforeEnter<br>
+            beforeRouteEnter<br>
+            beforeResolve<br>
+            afterEach<br>
+            beforeCreate<br>
+        </p>
+
+        
+        <h2>同一个路由参数更新钩子顺序</h2>
+        <p>
+            beforeEach<br>
+            beforeRouteUpdate<br>
+            beforeResolve<br>
+            afterEach<br>
+            updated<br>
+        </p>
+
+        <h2>切换为其他路由时</h2>
+        <p>
+            beforeRouteLeave<br>
+            beforeEach<br>
+            ...
+            afterEach<br>
+            beforeDestroy<br>
+        </p>
     </div>
 </template>
 
@@ -18,7 +47,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import User from '@/components/User';
-import UserPost from '@/components/UserPost';
+;
 import NotFound from '@/components/404';
 import Foo from '@/components/Foo2';
 
@@ -35,7 +64,12 @@ const router = new VueRouter({
                 next();
             }
         },
-        {path: '/user/:id/post/:postId', component: UserPost},
+        {path: '/user/:id/post/:postId', component: () => import('@/components/UserPost'),
+            beforeEnter(to, from, next){
+                console.log('beforeEnter');
+                next();
+            }
+        },
         {path: '/foo/:id', component: Foo, props: true},
     ]
 })
@@ -51,6 +85,11 @@ router.beforeEach(function(to, from, next){
     } else {
         next();
     }
+})
+
+router.beforeResolve(function(to, from, next){
+    console.log('beforeResolve');
+    next();
 })
 
 router.afterEach(function(to, from){
