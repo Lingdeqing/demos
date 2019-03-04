1138a7120ee2463ca0cc224e7ae5c4a076f640f8
yaolin1 <yaolin1@xiaomi.com>
2019-03-04 11:18:00 +0800

    WebComponents

--- react/demos/src/index.js
+++ react/demos/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './examples/ref透传-转发ref';
+import App from './examples/WebComponents';
 
 ReactDOM.render(<App />, document.getElementById('root'));
