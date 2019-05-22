d1e1765a0c08e307aa21a5a804bd1f0487be37af
yaolin1 <yaolin1@xiaomi.com>
2019-05-22 17:48:22 +0800

    错误处理

--- nodejs/koa/03_错误处理.js
+++ nodejs/koa/03_错误处理.js
@@ -6,6 +6,15 @@ app.use(async (ctx, next) => {
     if(ctx.url === '/error'){
         console.log(shit);
     }
+    if(ctx.url === '/error2'){
+        ctx.throw(400, 'error2 expose');
+    }
+    if(ctx.url.includes('/error3')){
+        const err = new Error('error3 expose');
+        err.status = 400;
+        err.expose = ctx.url.includes('expose');
+        throw err;
+    }
     ctx.body = `app.on("error")`;
 });
 
