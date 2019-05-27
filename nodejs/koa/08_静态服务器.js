32f1ddcbc9a3c0059c22fc906b91ac6608a23dee
yaolin1 <yaolin1@xiaomi.com>
2019-05-27 21:32:34 +0800

    静态服务器

--- nodejs/koa/08_静态服务器.js
+++ nodejs/koa/08_静态服务器.js
@@ -4,7 +4,28 @@ const fs = require('fs');
 const Koa = require('koa');
 const app = new Koa();
 
-const staticPath = './'
+const mimes = {
+  'css': 'text/css',
+  'less': 'text/css',
+  'gif': 'image/gif',
+  'html': 'text/html',
+  'ico': 'image/x-icon',
+  'jpeg': 'image/jpeg',
+  'jpg': 'image/jpeg',
+  'js': 'text/javascript',
+  'json': 'application/json',
+  'pdf': 'application/pdf',
+  'png': 'image/png',
+  'svg': 'image/svg+xml',
+  'swf': 'application/x-shockwave-flash',
+  'tiff': 'image/tiff',
+  'txt': 'text/plain',
+  'wav': 'audio/x-wav',
+  'wma': 'audio/x-ms-wma',
+  'wmv': 'video/x-ms-wmv',
+  'xml': 'text/xml'
+}
+const staticPath = '/workspace/private/test/demos/vue/vue-router/src'
 app.use(async (ctx, next) => {
     const filePath = path.join(staticPath, decodeURIComponent(ctx.url));
     let content = '404 not found'
@@ -13,14 +34,23 @@ app.use(async (ctx, next) => {
         if(stat.isDirectory()){
             const files = fs.readdirSync(filePath);
             content = files.map(file => {
-                const url = path.join(filePath, file);
-                return `<p><a href="${encodeURIComponent(url)}">${url}</a></p>`
+                const url = path.relative(staticPath, path.join(filePath, file));
+                return `<p><a href="${url}">${url}</a></p>`
             }).join('');
         } else {
+            const extname = path.extname(filePath);
+            ctx.type = mimes[extname ? extname.slice(1) : 'unknown'];
             content = fs.readFileSync(filePath, 'binary' );
         }
     } 
-    ctx.body = content;
+
+    if(ctx.type.indexOf('image') > -1){
+        ctx.res.writeHead(200);
+        ctx.res.write(content, 'binary');
+        ctx.res.end();
+    } else {
+        ctx.body = content;
+    }
 });
 
 app.listen(3000)
