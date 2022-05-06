ec265370bbfc69c9b3e57177c35500a29ad96ebe
姚林 <yaolin1@xiaomi.com>
2022-05-06 20:10:11 +0800

    位运算

---
+++ algorithm/剑指offer66题/40_数组中数字出现的次数.js
@@ -0,0 +1,37 @@
+/**
+    题目：
+        一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ */
+
+// time: 
+// space: 
+// 出现一次的2个数字，其他数字都出现两次
+function singleNumbers(nums) {
+    let res = 0
+    for (let num of nums) {
+        res ^= num
+    }
+    let m = 1;
+    while ((m & res) === 0) { // 说明res当前的bit位是0，说明 x⊕y 对应bit位相等
+        m <<= 1; // 不断向高位探测，直到bit位是1，也就是 x⊕y 对应bit位不相等
+    }
+    let x = 0, y = 0
+    for (let num of nums) {
+        if ((num & m) === 0) { // 对于bit位都是0的分到一组
+            x ^= num
+        } else { // 对于bit位都是1的分到另外一组
+            y ^= num
+        }
+    }
+    return [x, y]
+}
+
+// 出现一次的1个数字，其他数字都出现两次
+// 利用了 a⊕a=0，0⊕a=a，a⊕b=b⊕a => a⊕a⊕b⊕b⊕...⊕x=x
+function singleNumber(nums) {
+    let res = 0
+    for (let num of nums) {
+        res ^= num
+    }
+    return
+}
