f452dbb931655156a74cff1642356dfba01bb76f
yaolin <15851853085@163.com>
2022-10-12 20:31:22 +0800

    1

--- algorithm/CodeTop/141. 环形链表.js
+++ algorithm/CodeTop/141. 环形链表.js
@@ -6,9 +6,7 @@ function hasCycle(head) {
     while (fast && fast.next) {
         fast = fast.next.next
         slow = slow.next
-        if (fast === slow) {
-            return true
-        }
+        if (fast === slow) return true
     }
     return false
 }
