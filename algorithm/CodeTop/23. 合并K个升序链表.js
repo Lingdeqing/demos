e4ecb13cc4036c51d2a11c0334ada98672e6b897
yaolin <15851853085@163.com>
2022-10-24 18:52:33 +0800

    1

--- algorithm/CodeTop/23. 合并K个升序链表.js
+++ algorithm/CodeTop/23. 合并K个升序链表.js
@@ -2,22 +2,16 @@
 // https://leetcode.cn/problems/merge-k-sorted-lists/
 
 import PriorityQueue from "../lib/PriorityQueue"
+
 function mergeKLists(lists) {
     const pq = new PriorityQueue((a, b) => a.val < b.val)
-    lists.forEach((node) => {
-        if (node) {
-            pq.add(node)
-        }
-    })
-
-    let res = { next: null }, h = res
+    lists.forEach(list => list && pq.add(list))
+    let res = { next: null }, p = res
     while (!pq.isEmpty()) {
-        const node = pq.poll()
-        if (node.next) {
-            pq.add(node.next)
-        }
-        h.next = node;
-        h = h.next;
+        const list = pq.poll()
+        if (list.next) pq.add(list.next)
+        p.next = list;
+        p = p.next
     }
     return res.next
 }
