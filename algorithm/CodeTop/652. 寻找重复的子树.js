d21d759c8f8a3d0a9f43eaa13c8ffe9175d7f28b
yaolin <15851853085@163.com>
2022-11-15 22:04:34 +0800

    1

--- algorithm/CodeTop/652. 寻找重复的子树.js
+++ algorithm/CodeTop/652. 寻找重复的子树.js
@@ -2,18 +2,16 @@
 // https://leetcode.cn/problems/find-duplicate-subtrees/
 
 // 二叉树序列化
-function findDuplicateSubtrees(root) {
-    let map = new Map(), res = []
-    function dfs(root) {
-        if (!root) return '#'
-        const left = dfs(root.left)
-        const right = dfs(root.right)
-        const subTree = left + ',' + right + ',' + root.val
-        map.set(subTree, (map.get(subTree) || 0) + 1)
-        if (map.get(subTree) === 2) {
-            res.push(root)
+var findDuplicateSubtrees = function findDuplicateSubtrees(root) {
+    const res = [], map = new Map()
+    function dfs(node) {
+        if (!node) return '#'
+        const key = [dfs(node.left), dfs(node.right), node.val].join(',')
+        if (map.get(key) === 1) {
+            res.push(node)
         }
-        return subTree
+        map.set(key, (map.get(key) || 0) + 1)
+        return key
     }
     dfs(root)
     return res
