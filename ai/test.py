f1c763c55407c90f67aa49be4468b69f2d7e9c02
yaolin <15851853085@163.com>
2023-07-24 19:57:00 +0800

    1

---
+++ ai/test.py
@@ -0,0 +1,41 @@
+import openai
+
+# 读取系统中的环境变量
+openai.api_key  = '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'
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    '''
+    prompt: 对应的提示
+    model: 调用的模型，默认为 gpt-3.5-turbo(ChatGPT)，有内测资格的用户可以选择 gpt-4
+    '''
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # 模型输出的温度系数，控制输出的随机程度
+    )
+    # 调用 OpenAI 的 ChatCompletion 接口
+    return response.choices[0].message["content"]
+
+# 中文版见下一个 cell
+text = f"""
+You should express what you want a model to do by \ 
+providing instructions that are as clear and \ 
+specific as you can possibly make them. \ 
+This will guide the model towards the desired output, \ 
+and reduce the chances of receiving irrelevant \ 
+or incorrect responses. Don't confuse writing a \ 
+clear prompt with writing a short prompt. \ 
+In many cases, longer prompts provide more clarity \ 
+and context for the model, which can lead to \ 
+more detailed and relevant outputs.
+"""
+prompt = f"""
+Summarize the text delimited by triple backticks \ 
+into a single sentence.
+```{text}```
+"""
+response = get_completion(prompt)
+print('11')
+print(response)
