da9ec3d295911379f8048bf529d34f509ef901d2
yaolin <lin.yao@mobvoi.com>
2024-08-29 20:25:17 +0800

    1

---
+++ java/hello-servlet/src/main/java/com/yaolin/www/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.yaolin.www.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>hello, " + (name != null ? name : "nobody") + "</h1>");
+        writer.flush();
+    }
+}
