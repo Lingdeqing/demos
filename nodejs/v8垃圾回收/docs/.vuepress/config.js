2b9b1a4f219a7e6c7bc5de7c2d7c71e6acf1ffff
yaolin <lin.yao@mobvoi.com>
2024-12-19 17:12:54 +0800

    1

---
+++ nodejs/v8垃圾回收/docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'V8引擎中的垃圾收集介绍',
+  description: 'Just playing around',
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'JavaScript中的垃圾收集',
+        path: '/guide/',
+        collapsable: false,
+        sidebarDepth: 1,
+      },
+      {
+        title: 'V8的垃圾回收机制',
+        path: '/reactivity/',
+        collapsable: false,
+        sidebarDepth: 1,
+      }
+    ],
+  },
+}
---
+++ nodejs/v8垃圾回收/docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: null
+heroText: V8引擎中垃圾收集
+tagline: 垃圾收集简单介绍
+actionText: 进入文档 →
+actionLink: /guide/
