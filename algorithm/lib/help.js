651d667e29d6c78e9e4ec22721cb713a54771e60
yaolin <lin.yao@mobvoi.com>
2025-03-04 11:54:48 +0800

    1

--- algorithm/lib/help.js
+++ algorithm/lib/help.js
@@ -9,3 +9,32 @@ function list(arr) {
     }
     return x.next
 }
+
+function deserializeArray(nodes) {
+    const root = {
+        val: nodes.shift()
+    }
+    const queue = [root]
+    while (queue.length && nodes.length) {
+        const node = queue.shift()
+
+        const leftVal = nodes.shift()
+        if (leftVal !== null) {
+            const leftNode = {
+                val: leftVal
+            }
+            node.left = leftNode
+            queue.push(leftNode)
+        }
+
+        const rightVal = nodes.shift()
+        if (rightVal !== null) {
+            const rightNode = {
+                val: rightVal
+            }
+            node.right = rightNode
+            queue.push(rightNode)
+        }
+    }
+    return root
+}
--- algorithm/力扣热题100/416_分割等和子集.js
+++ algorithm/力扣热题100/416_分割等和子集.js
@@ -2,17 +2,17 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+// 转换成背包问题
+// 给一个可装载重量为 sum / 2 的背包和 N 个物品，每个物品的重量为 nums[i]。现在让你装物品，是否存在一种装法，能够恰好将背包装满？
 var canPartition = function (nums) {
     let sum = nums.reduce((s, n) => s += n, 0)
     if (sum % 2 !== 0) return false
     sum /= 2
 
     // dp[i][j] 表示nums[0...i]中是否可以刚好凑出j的质量
+    // dp[i][j] 表示[0...i]是否取一部分可以正好凑出j重量
     const dp = Array.from({ length: nums.length }, () => new Array(sum + 1).fill(false))
-    // 边界：凑出0重量
-    for (let i = 0; i < nums.length; i++) {
-        dp[i][0] = true
-    }
     for (let i = 0; i < nums.length; i++) {
         for (let j = 1; j <= sum; j++) {
             if (i === 0) {
