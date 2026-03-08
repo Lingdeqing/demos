4b3d6527de1301c2d9ad58c688734885d5e4c48c
Frank <Frank@MacBook-Pro.local>
2026-03-08 15:18:04 +0800

    1

--- react/从零构建react应用/my-app/src/main.tsx
+++ react/从零构建react应用/my-app/src/main.tsx
@@ -5,9 +5,10 @@ import { createRoot } from 'react-dom/client'
 // import TestEvent from './components/TestEvent.tsx'
 import TestSetState from './components/TestSetState.tsx'
 import TestContext from './components/TestContext.tsx'
+import TestUseDeferredValue from './components/TestUseDeferredValue.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TestContext />
+    <TestUseDeferredValue />
   </StrictMode>,
 )
