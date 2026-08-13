0d959a14ff4104eaa5d0aeb6195f88d3348d5fbe
yaolin <yaolin@felicitysolar.com>
2026-08-13 11:23:02 +0800

    1

--- algorithm/力扣热题100/0_不熟.ts
+++ algorithm/力扣热题100/0_不熟.ts
@@ -73,3 +73,19 @@
 
 
 
+function wordBreak(s: string, wordDict: string[]): boolean {
+    let memo = new Map<number, boolean>()
+    function dp(start: number):boolean{
+        if(start >= s.length) return true
+        if(memo.has(start)) return memo.get(start)!
+        for(let word of wordDict){
+            if(s.startsWith(word, start) && dp(start + word.length)){
+                memo.set(start, true)
+                return true
+            }
+        }
+        memo.set(start, false)
+        return false
+    }
+    return dp(0)
+};
